c8dab2cd12b28e9eaa7c3ba649ff62413b98cd38
greaterbookwyrm <samfriedmen@gmail.com>
2025-04-19 23:16:00 -0700

    Update problem3.py
    I hate math so much why did you do this to me?

--- Mod2/problem3.py
+++ Mod2/problem3.py
@@ -1,1 +1,7 @@
-
+import math
+a = float (input("What is the first triangle side?"))
+b = float (input("What is the second triangle side?"))
+c = float (input("What is the third triangle side?"))
+s = .5*(a+b+c)
+squarethis = s*(s-a)*(s-b)*(s-c)
+print(math.sqrt(squarethis))
