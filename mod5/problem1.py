80389d964a5ba183f92e0cb0bd1180df72fad338
greaterbookwyrm <samfriedmen@gmail.com>
2025-05-11 22:10:48 -0700

    Create problem1.py
    problem solved

---
+++ mod5/problem1.py
@@ -0,0 +1,10 @@
+number = int(input("what is your favorite positive number?"))
+counter = 1
+sum = 0
+
+while counter <= number:
+    sum += counter
+    counter += 1
+print("the sum of all numbers up to your favorite number added together is;", sum)
+
+# I feel like my math wording is weird, but I think the point is made haha
