69524b29659ac0b4e3971c0b8302ba15f0ce58d0
greaterbookwyrm <samfriedmen@gmail.com>
2025-05-18 15:24:10 -0700

    Create problem1.py
    Problem solve

---
+++ mod6/problem1.py
@@ -0,0 +1,4 @@
+##1. Create a list of integers (you get to pick!). Write code to iterate through the list and calculate the sum of all even numbers. Print the resulting sum.
+# I think this is right? I am not sure what "Iterate through the list" means? 
+pi = [3,141,5,92,6,53,5,8,97,9,3,238,46,26,4,3,383,279,5,2,8,41,97,1] 
+print(sum(pi))
