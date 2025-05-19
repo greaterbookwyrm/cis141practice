b33d2046c2ccaf3bcb661f75f73ade716a73b921
greaterbookwyrm <samfriedmen@gmail.com>
2025-05-18 17:37:30 -0700

    Create problem2.py
    problem solve

---
+++ mod6/problem2.py
@@ -0,0 +1,30 @@
+random = [
+    "Lorem",
+    "ipsum",
+    "dolor",
+    "sit",
+    "Olympic",
+    "Olympic",
+    "amet",
+    "consectetur",
+    "adipiscing",
+    "elit",
+    "Quisque",
+    "faucibus",
+    "ex",
+    "sapien",
+    "vitae",
+    "pellentesque",
+    "sem",
+    "placerat",
+    "Olympic",
+    "Olympic",
+    "Olympic",
+    "Olympic",
+    "Olympic",
+]
+counter = 0
+for item in random:
+    if item == "Olympic":
+        counter += 1
+print(counter)
